c1bd8606b87c9dc8cbcde150c000d1079531d868
Val Do <valeri.kharitonashvili1@gmail.com>
2026-01-23 00:29:41 +0400

    mason and mason lsp config

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason-lspconfig").setup {
+            ensure_installed = { "lua_ls", "rust_analyzer" },
+        }
+    end
+}
