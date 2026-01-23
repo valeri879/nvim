57251d7c2aeb6d01df511c932765730cecb6e7d2
Val Do <valeri.kharitonashvili1@gmail.com>
2026-01-23 14:25:55 +0400

    telescope nvim and comments.lua

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live grep" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help tags" },
+    },
+    opts = {}
+}
