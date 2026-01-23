338ed5bc919857e696f11c667630dd1a616e7912
Val Do <valeri.kharitonashvili1@gmail.com>
2026-01-23 18:49:40 +0400

    lsp

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,13 +1,23 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
     },
-    config = function()
-        require("mason-lspconfig").setup {
-            ensure_installed = { "lua_ls", "rust_analyzer" },
-        }
+    opts = {
+        ensure_installed = { "lua_ls", "rust_analyzer" },
+        -- 'automatic_enable' is true by default in v2.0+
+    },
+    config = function(_, opts)
+        require("mason-lspconfig").setup(opts)
+
+        -- Mapping "Go to Definition" via the modern LspAttach autocommand
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(ev)
+                local b_opts = { buffer = ev.buf }
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, b_opts)
+            end,
+        })
     end
 }
+
