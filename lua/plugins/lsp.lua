583c71bcfb69accbc40cb4afbc9542bafb55f833
Val Do <valeri.kharitonashvili1@gmail.com>
2026-02-02 00:16:18 +0400

    LSP for angular and some fixes

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,37 @@
+return {
+    vim.lsp.config('angularls', {
+        cmd = (function()
+            local root = vim.fs.root(0, { 'package.json', 'angular.json' })
+
+            -- Construct the path to the index.js file
+            local ngserver = root and (root .. "/node_modules/@angular/language-server/bin/ngserver")
+            local node_modules = root and (root .. "/node_modules")
+
+            if ngserver and vim.fn.filereadable(ngserver) == 1 then
+                return {
+                    ngserver,
+                    "--stdio",
+                    "--tsProbeLocations", node_modules,
+                    "--ngProbeLocations", node_modules
+                }
+            end
+
+            -- Fallback to global command if local isn't found
+            return { "ngserver", "--stdio" }
+        end)(),
+        filetypes = { 'typescript', 'html', 'htmlangular' },
+        root_markers = { 'package.json', 'angular.json', '.git' },
+    });
+
+    vim.lsp.enable('angularls');
+
+    -- 3. The "Force Fix" for HTML files
+    -- This ensures that when an HTML file is opened, Neovim checks for the Angular LSP
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "html", "typescript" },
+        callback = function()
+            -- This checks if angularls should be attached to this specific buffer
+            vim.lsp.enable('angularls')
+        end,
+    })
+}
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -5,8 +5,7 @@ return {
         "neovim/nvim-lspconfig",
     },
     opts = {
-        ensure_installed = { "lua_ls", "rust_analyzer" },
-        -- 'automatic_enable' is true by default in v2.0+
+        ensure_installed = { "lua_ls", "angularls" },
     },
     config = function(_, opts)
         require("mason-lspconfig").setup(opts)
@@ -14,10 +13,38 @@ return {
         -- Mapping "Go to Definition" via the modern LspAttach autocommand
         vim.api.nvim_create_autocmd('LspAttach', {
             callback = function(ev)
-                local b_opts = { buffer = ev.buf }
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, b_opts)
+                local opts = { buffer = ev.buf }
+                vim.diagnostic.config({
+                    virtual_text = true,           -- Show error message next to the code
+                    signs = true,                  -- Show icons in the gutter
+                    update_in_insert = false,      -- Don't update diagnostics while typing (prevents flickering)
+                    underline = true,
+                    severity_sort = true,
+                    float = {
+                        border = 'rounded',        -- Add a nice border to the error popup
+                        source = 'always',         -- Show which LSP the error came from (e.g., angularls)
+                    },
+                })
+                -- Navigation
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)     -- Go to definition
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)    -- Go to declaration
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)     -- List all references
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts) -- Go to implementation
+
+                -- Documentation / Info
+                vim.keymap.set('n', 'K',  vim.lsp.buf.hover, opts)          -- Show documentation (Hover)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts) -- Signature help
+
+                -- Actions
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)      -- Rename symbol
+                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- Code actions
+
+                -- Diagnostics (Errors/Warnings)
+                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)   -- Previous error
+                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)   -- Next error
+                vim.keymap.set('n', '<leader>i', vim.diagnostic.open_float, opts) -- Show error in popup
             end,
         })
+
     end
 }
-
